d4c993450d4f6296cc40e67cba1de77cf3d76d7e
Your Name <you@example.com>
2024-04-22 01:12:40 +0300

    Updated Loading Animations

--- app/loading.tsx
+++ app/loading.tsx
@@ -2,7 +2,13 @@ import LoadingAnime from "@/components/LoadingAnime";
 import React from "react";
 
 const loading = () => {
-  return <LoadingAnime />;
+  return (
+    <div className="min-h-[100vh]">
+      <div className="flex justify-center items-center">
+        <LoadingAnime />
+      </div>
+    </div>
+  );
 };
 
 export default loading;
