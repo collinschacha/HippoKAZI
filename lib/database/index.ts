a3a9294ca6061dae9b1b7db6e5a888355ab5b02a
Your Name <you@example.com>
2024-04-24 13:08:59 +0300

    commit

--- lib/database/index.ts
+++ lib/database/index.ts
@@ -4,8 +4,6 @@ const MONGODB_URI = process.env.MONGODB_URI;
 export const dynamic = "force-dynamic";
 let cached = (global as any).mongoose || { conn: null, promise: null };
 export const connectToDatabase = async () => {
-  if (cached.conn) return cached.conn;
-
   if (!MONGODB_URI) throw new Error("MONGODB_URI is not defined");
 
   cached.promise =
